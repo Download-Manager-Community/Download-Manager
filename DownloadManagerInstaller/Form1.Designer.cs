08b6a45bc9228820a73df70e764a9bebac90d067
Soniczac7 <Soniczac7@Gmail.com>
2022-07-06 13:13:42 +0100

    Changed order of controls

--- DownloadManagerInstaller/Form1.Designer.cs
+++ DownloadManagerInstaller/Form1.Designer.cs
@@ -301,14 +301,14 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(523, 374);
-            this.Controls.Add(this.checkBox4);
             this.Controls.Add(this.checkBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.checkBox4);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.tabControl2);
-            this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
