9a434475269561259c70a33048f180f09020c7f6
Soniczac7 <Soniczac7@Gmail.com>
2022-08-28 17:16:13 +0100

    Added download complete sound effect

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -81,6 +81,15 @@ namespace DownloadManager.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
+        /// </summary>
+        internal static System.IO.UnmanagedMemoryStream complete {
+            get {
+                return ResourceManager.GetStream("complete", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap error {
