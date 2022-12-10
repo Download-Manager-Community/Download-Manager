2f49d67ec27229a812b18a43e471bf862b4d6fe5
Soniczac7 <Soniczac7@Gmail.com>
2022-12-10 21:32:55 +0000

    Make everything in `BrowserIntercept` more reliable.

--- BrowserIntercept.cs
+++ BrowserIntercept.cs
@@ -54,19 +54,95 @@ namespace DownloadManager
                 Log("Internal server ready. Listening for connections on port " + serverPort + ".", Color.Green);
                 Socket client = httpServer.Accept();
 
+                Log("Reading inbound connection data.", Color.Gray);
+
+                bool timeOut = false;
+                bool nullBytes = false;
+
                 // Read inbound connection data
                 while (true)
                 {
-                    int numBytes = client.Receive(bytes);
-                    data += Encoding.ASCII.GetString(bytes, 0, numBytes);
+                    if (client.Poll(1000, SelectMode.SelectWrite) == false)
+                    {
+                        Log("No data is available to be read and will be ignored.", Color.Orange);
+                        break;
+                    }
+                    // Use a 5 second timeout
+                    int? numBytes = null;
 
-                    if (data.IndexOf("\r\n") > -1)
+                    Thread thread = new Thread(new ThreadStart(delegate ()
+                    {
+                        try
+                        {
+                            numBytes = client.Receive(bytes);
+                        }
+                        catch (SocketException ex)
+                        {
+                            if (ex.Message == "An established connection was aborted by the software in your host machine.")
+                            {
+                                Log("The connection was canceled by the browser!", Color.Orange);
+                            }
+                            else
+                            {
+                                Log(ex.Message + " (" + ex.GetType() + ")" + Environment.NewLine + ex.StackTrace, Color.Red);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Log(ex.Message + " (" + ex.GetType() + ")" + Environment.NewLine + ex.StackTrace, Color.Red);
+                        }
+                    }));
+                    thread.Start();
+                    if (!thread.Join(TimeSpan.FromSeconds(2)))
+                    {
+                        try
+                        {
+                            thread.Abort();
+                        }
+                        catch { }
+                        Log("Download Manager timed-out while reading inbound connection data. The connection will be ignored.", Color.Orange);
+                        timeOut = true;
+                        break;
+                    }
+
+                    if (numBytes == null)
+                    {
+                        nullBytes = true;
+                        break;
+                    }
+
+                    data += Encoding.ASCII.GetString(bytes, 0, Convert.ToInt32(numBytes));
+
+                    if (data.IndexOf("\r\n") > -1 || data == "")
                     {
                         break;
                     }
                 }
 
+                Log("Finished reading inbound connection data.", Color.Gray);
+
+                if (timeOut)
+                {
+                    Log("Failed to read inbound connection data. Receiving data timed-out!", Color.Red);
+                    client.Close();
+                    bytes = null;
+                    continue;
+                }
+
+                if (nullBytes)
+                {
+                    Log("Failed to read inbound connection data. The number of bytes was null!", Color.Red);
+                    client.Close();
+                    bytes = null;
+                    continue;
+                }
+
                 string[] splittedData = data.Split(new string[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                if (splittedData.Length <= 0)
+                {
+                    Log("Received empty request. Ignoring...", Color.Orange);
+                    continue;
+                }
                 string url = splittedData[0].ToString().Replace("%22 HTTP/1.1", "");
                 url = url.Replace("GET /?url=%22", "");
 
@@ -90,14 +166,12 @@ namespace DownloadManager
                 }
 
                 string resHeader = "HTTP/1.1 200 OK\nServer: DownloadManager_Internal\nContent-Type: text/html; charset: UTF-8\n\n";
-                string resBody = "<!DOCTYPE html><html><head><title>Download Manager</title><style>body{background-color: rgb(18, 22, 58);color: white;font-family: Arial, Helvetica, sans-serif;}h1{font-size: 30px;}</style></head><body><h1>Download Manager</h1><hr><p>Please do not manually send requests to the internal server.</p><p>If a request is sent incorrectly the internal server may crash.If this occurs please restart the Download Manager application.</p></body></html>";
+                string resBody = "<!DOCTYPE html><html><head><title>Download Manager</title><style>body{background-color: rgb(18, 22, 58);color: white;font-family: Arial, Helvetica, sans-serif;}h1{font-size: 30px;}</style></head><body><h1>Download Manager</h1><hr><p>Please do not manually send requests to the internal server.</p><p>This may cause Download Manager to behave unexpectedly.</p></body></html>";
                 string resStr = resHeader + resBody;
                 byte[] resData = Encoding.ASCII.GetBytes(resStr);
                 client.SendTo(resData, client.RemoteEndPoint);
                 client.Close();
                 bytes = null;
-                httpServer.Close();
-                ConnectionThreadMethod();
             }
         }
     }
